4e5897f36919b892dae0ae3c0cfd775705d32956
Ahmustufa <ahmedmustafa1234@gmail.com>
2022-01-06 17:42:59 +0500

    changes

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
-
-import './App.css';
-import Header from './components/Header';
-import Home from './screens/Home';
-import Footer from './components/Footer'
+import "./App.css";
+import Header from "./components/Header";
+import Home from "./screens/Home";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <>
-    <Header/>
-    <Home/>
-    <Footer/>
+      <Header />
+      <Home />
+      <Footer />
     </>
-    
   );
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,36 +1,40 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import { Container, Nav, Navbar, NavDropdown } from 'react-bootstrap'
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import {  faEnvelope,faUser } from '@fortawesome/free-solid-svg-icons'
-import {  faFacebookF,faTwitter,faLinkedin, faPinterest } from '@fortawesome/free-brands-svg-icons'
-import '../ComponentStyles/Header.css'
-
+import React from "react";
+import ReactDOM from "react-dom";
+import { Container, Nav, Navbar, NavDropdown } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEnvelope, faUser } from "@fortawesome/free-solid-svg-icons";
+import {
+  faFacebookF,
+  faTwitter,
+  faLinkedin,
+  faPinterest,
+} from "@fortawesome/free-brands-svg-icons";
+import "../ComponentStyles/Header.css";
 
 const header = () => {
-    return (
-        <div>
-            <Navbar bg="light" expand="lg">
-  <Container>
-    <Navbar.Brand href="#home">hello@colorlib.com</Navbar.Brand>
-    <Navbar.Toggle aria-controls="basic-navbar-nav" />
-    <Navbar.Collapse id="basic-navbar-nav">
-      <Nav className="me-auto">
-        <Nav.Link href="#link">| </Nav.Link>
-        <Nav.Link href="#link">Free Shipping for all Order of $99 </Nav.Link>
-        
-      </Nav>
-      <FontAwesomeIcon className='pin' icon={faPinterest} />
-      <FontAwesomeIcon className='fac' icon={faFacebookF} />
-      <FontAwesomeIcon className='twi' icon={faTwitter} />
-      <FontAwesomeIcon className='Link' icon={faLinkedin} />
+  return (
+    <div>
+      <Navbar bg="light" expand="lg">
+        <Container>
+          <Navbar.Brand href="#home">hello@colorlib.com</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="#link">| </Nav.Link>
+              <Nav.Link href="#link">
+                Free Shipping for all Order of $99{" "}
+              </Nav.Link>
+            </Nav>
+            <FontAwesomeIcon className="pin" icon={faPinterest} />
+            <FontAwesomeIcon className="fac" icon={faFacebookF} />
+            <FontAwesomeIcon className="twi" icon={faTwitter} />
+            <FontAwesomeIcon className="Link" icon={faLinkedin} />
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </div>
+  );
+};
 
-    </Navbar.Collapse>
-  </Container>
-</Navbar>
-        </div>
-    )
-}
-
-export default header
+export default header;
