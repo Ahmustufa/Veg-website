b647a053ec5212e6c171f48562ac34e11eca83e0
Ahmustufa <ahmedmustafa1234@gmail.com>
2022-01-12 23:43:39 +0500

    ui changes

--- src/screens/Blog.js
+++ src/screens/Blog.js
@@ -1,5 +1,7 @@
 import React from "react";
 import Helmet from "react-helmet";
+import { Link } from "react-router-dom";
+import "../ScreenStyles/Blog.css"
 export const Blog = () => {
   return (
     <>
@@ -8,7 +10,12 @@ export const Blog = () => {
         <title>Blog</title>
         <link rel="canonical" href="http://mysite.com/example" />
       </Helmet>
-      <div>Blog</div>
+      <div className="bg-div mb-5">
+        <h1>Organi Shop</h1>
+        <Link to={"/"} className="bg-link">
+          Home - Contact Us
+        </Link>
+      </div>
     </>
   );
 };
